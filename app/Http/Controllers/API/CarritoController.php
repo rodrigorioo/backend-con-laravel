08e5f79afe6805f81a3a531f271d1091afdd616e
Rodrigo Rio <rodrigo.rio@outlook.com.ar>
2023-08-02 11:14:13 -0300

    + Mailpit

--- app/Http/Controllers/API/CarritoController.php
+++ app/Http/Controllers/API/CarritoController.php
@@ -34,8 +34,7 @@ class CarritoController extends Controller
 
     public function finalizarCompra(FinalizarCompraAPIRequest $request) {
 
-        // Forma 1
-        $preferencia = DB::transaction(function() use($request) {
+        [$compra, $preferencia] = DB::transaction(function() use($request) {
 
             // Crear la compra
             $compra = $this->compraService->crearCompra($request);
@@ -46,41 +45,11 @@ class CarritoController extends Controller
             // Ejecutar evento de compra finalizada
             CompraFinalizada::dispatch($compra);
 
-            return $preferencia;
+            return [$compra, $preferencia];
         });
 
-        // Forma 2
-//        DB::beginTransaction();
-//
-//        // Crear la compra
-//        try {
-//            $compra = $this->compraService->crearCompra($request);
-//        } catch (\Exception $e) {
-//            DB::rollBack();
-//
-//            return new JsonResponse([
-//                'mensaje' => $e->getMessage(),
-//            ], 500);
-//        }
-//
-//        // Integrar mercadopago
-//        try {
-//            $preferencia = $this->mercadoPagoService->crearPreferencia($compra);
-//        } catch (\Exception $e) {
-//            DB::rollBack();
-//
-//            return new JsonResponse([
-//                'mensaje' => $e->getMessage(),
-//            ], 500);
-//        }
-//
-//        // Ejecutar evento de compra finalizada
-//        CompraFinalizada::dispatch($compra);
-//
-//        DB::commit();
-
         // Enviar los mails
-        // $this->compraService->enviarMail($compra, $preferencia->init_point);
+        $this->compraService->enviarMail($compra, $preferencia->init_point);
 
         return new JsonResponse([
             'mensaje' => 'Compra finalizada',
